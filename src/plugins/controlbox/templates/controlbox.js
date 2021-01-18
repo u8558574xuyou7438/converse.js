b7efed1692c9ddfeae4ce039f6e71d567911a381
JC Brand <jc@opkode.com>
2021-01-18 16:20:08 +0100

    Componentize dragresize

--- src/plugins/controlbox/templates/controlbox.js
+++ src/plugins/controlbox/templates/controlbox.js
@@ -3,6 +3,7 @@ import { api } from '@converse/headless/core';
 
 export default (o) => html`
     <div class="flyout box-flyout">
+        <converse-dragresize></converse-dragresize>
         <div class="chat-head controlbox-head">
             ${o.sticky_controlbox ? '' : html`<a class="chatbox-btn close-chatbox-button fa fa-times"></a>` }
         </div>
